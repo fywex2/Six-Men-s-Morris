0dd0c893c90aeba25ba6b5c92758cfa5a590df7f
Fywex <megastarsgaming@gmail.com>
2023-11-24 17:13:43 +0200

    few fixes and adding
    fixed few mistakes and added stuff for the data collector

--- JsonMaking.py
+++ JsonMaking.py
@@ -24,20 +24,25 @@ class Game_NineMensMorris:
         self.opp_pieces_not_placed = 9  # the amount of pieces that wasn't place on the board of the opponent
         self.white_mills = 0  # white mills on the board
         self.black_mills = 0  # black mills on the board
+        self.win_points_agent = 1  # points for the win of agent
+        self.tie_points = 0.5  # points for a tie
+        self.loss_points_agent = 0  # points for a loss of agnet
+        self.states = []  # collecting states from a single game
+        self.state_score = []  # collecting scores for each state in the game
+        self.gama = 0.9  # amount to multiply the state every new board
 
     # returns a list of where pieces could be placed
     def legal_places_before(self):
-        tupple_list = []
+        move_list = []
         for i in range(7):
             for j in range(7):
                 if self.board[i][j] == 0:
-                    tupple_list.append((i, j))
-        return tupple_list
+                    move_list.append((i, j))
+        return move_list
 
     # returns a list of where pieces could be moved to after all the pieces of the player where put on the board
     def legal_places_after(self, player):
-        moves = []
-
+        moves_list = []
         for i in range(7):
             for j in range(7):
                 if self.board[i, j] == player:
@@ -45,14 +50,12 @@ class Game_NineMensMorris:
 
                     for adj_i, adj_j in adjacent_positions:
                         if (0 <= adj_i < 7) and (0 <= adj_j < 7) and self.board[adj_i, adj_j] == 0:
-                            moves.append(((i, j), (adj_i, adj_j)))
-
-        return moves
+                            moves_list.append(((i, j), (adj_i, adj_j)))
+        return moves_list
 
     # returns a list of where pieces could be moved when there are 3 pieces left on the board (flying stage)
     def flying_stage_moves(self, player):
         moves = []
-
         for i in range(7):
             for j in range(7):
                 if self.board[i][j] == player:
@@ -60,26 +63,25 @@ class Game_NineMensMorris:
                         for y in range(7):
                             if self.board[x][y] == 0 and (i == x or j == y or abs(i - x) == abs(j - y)):
                                 moves.append(((i, j), (x, y)))
-
         return moves
 
     # returns a list of where the white pieces are on the board
     def white_places(self):
-        white_tuple = []
+        white_locations = []
         for i in range(7):
             for j in range(7):
                 if self.board[i][j] == 1:
-                    white_tuple.append((i,j))
-        return white_tuple
+                    white_locations.append((i,j))
+        return white_locations
 
     # returns a list of where the black pieces are on the board
     def black_places(self):
-        black_tuple = []
+        black_locations = []
         for i in range(7):
             for j in range(7):
                 if self.board[i][j] == 2:
-                    black_tuple.append((i,j))
-        return black_tuple
+                    black_locations.append((i,j))
+        return black_locations
 
     # checks if there is a new line of 3 pieces of the selected player
     def check_new_mills(self, player):
@@ -87,27 +89,23 @@ class Game_NineMensMorris:
         for i in range(7):
             if self.board[i, 0] == self.board[i, 1] == self.board[i, 2] == player:
                 count += 1
-
             if self.board[0, i] == self.board[1, i] == self.board[2, i] == player:
                 count += 1
-
         if player == 1:
             if count > self.white_mills:
                 self.white_mills = count
                 return True
-
         if player == 2:
             if count > self.black_mills:
                 self.black_mills = count
                 return True
-
         return False
 
     # return true if the parameter player have won and false else
     def check_winner(self):
-        if self.opp_pieces_not_placed == 0 and not bool(self.legal_places_after(2)) or self.agent_pieces < 3:
+        if (self.opp_pieces_not_placed == 0 and not bool(self.legal_places_after(2))) or self.agent_pieces < 3:
             return 1
-        if self.agent_pieces_not_placed == 0 and not bool(self.legal_places_after(1)) or self.opp_pieces < 3:
+        if (self.agent_pieces_not_placed == 0 and not bool(self.legal_places_after(1))) or self.opp_pieces < 3:
             return 2
         return 0
 
@@ -165,7 +163,10 @@ class Game_NineMensMorris:
     def opp_turn(self):
         if self.opp_pieces == 3 and self.opp_pieces_not_placed == 0:
             legal = self.flying_stage_moves(2)
-            random_move = legal[rnd.randint(0, len(legal)-1)]
+            if len(legal) < 2:
+                random_move = legal[0]
+            else:
+                random_move = legal[rnd.randint(0, len(legal) - 1)]
             self.board[random_move[0][0]][random_move[0][1]] = 0
             self.board[random_move[1][0]][random_move[1][1]] = 2
             return
