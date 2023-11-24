082adb8169125c38fa231ea752bd0aaf4c12db6a
Fywex <megastarsgaming@gmail.com>
2023-11-24 14:44:11 +0200

    update
    fixing bugs

--- JsonMaking.py
+++ JsonMaking.py
@@ -18,19 +18,12 @@ class game_NineMensMorris:
             [-1, 0, -1, 0, -1, 0, -1],
             [0, -1, -1, 0, -1, -1, 0]
             ], dtype=np.int8)
-        self.agent_pieces = 9 # the amount of total pieces of the agent
-        self.opp_pieces = 9 # the amount of total pieces of the opponent
-        self.agent_pieces_notplaced = 9 # the amount of pieces that wasn't place on the board of the agent
-        self.opp_pieces_notplaced = 9 # the amount of pieces that wasn't place on the board of the opponent
-        self.white_mills = 0 # white mills on the board
-        self.black_mills = 0 # black mills on the board
-        self.win_points_agent = 1  # it is recommended to give 1 or 10
-        self.tie_points = 0.5  # smaller from win points
-        self.loss_points_agent = 0
-        self.all_boards_in_game = []  # only number sting
-        self.gama = 0.9  # he called discorent how much down we want to get between board (0.9 to 1)
-        self.states = []
-        self.statesBoards = []
+        self.agent_pieces = 9  # the amount of total pieces of the agent
+        self.opp_pieces = 9  # the amount of total pieces of the opponent
+        self.agent_pieces_not_placed = 9  # the amount of pieces that wasn't place on the board of the agent
+        self.opp_pieces_not_placed = 9  # the amount of pieces that wasn't place on the board of the opponent
+        self.white_mills = 0  # white mills on the board
+        self.black_mills = 0  # black mills on the board
 
     # returns a list of where pieces could be placed
     def legal_places_before(self):
@@ -41,7 +34,7 @@ class game_NineMensMorris:
                     tupple_list.append((i, j))
         return tupple_list
 
-    # returns a list of where pieces could be moved to after all of the pieces of the player where put on the board
+    # returns a list of where pieces could be moved to after all the pieces of the player where put on the board
     def legal_places_after(self, player):
         moves = []
 
@@ -57,15 +50,15 @@ class game_NineMensMorris:
         return moves
 
     # returns a list of where pieces could be moved when there are 3 pieces left on the board (flying stage)
-    def flying_stage_moves(board, player):
+    def flying_stage_moves(self, player):
         moves = []
 
         for i in range(7):
             for j in range(7):
-                if board[i][j] == player:
+                if self.board[i][j] == player:
                     for x in range(7):
                         for y in range(7):
-                            if board[x][y] == 0 and (i == x or j == y or abs(i - x) == abs(j - y)):
+                            if self.board[x][y] == 0 and (i == x or j == y or abs(i - x) == abs(j - y)):
                                 moves.append(((i, j), (x, y)))
 
         return moves
@@ -92,12 +85,10 @@ class game_NineMensMorris:
     def check_new_mills(self, player):
         count = 0
         for i in range(7):
-            if (self.board[i, 0] == self.board[i, 1] == self.board[i, 2] == player) or \
-                    (self.board[i, 0] == self.board[i, 1] == self.board[i, 2] == player):
+            if self.board[i, 0] == self.board[i, 1] == self.board[i, 2] == player:
                 count += 1
 
-            if (self.board[0, i] == self.board[1, i] == self.board[2, i] == player) or \
-                    (self.board[0, i] == self.board[1, i] == self.board[2, i] == player):
+            if self.board[0, i] == self.board[1, i] == self.board[2, i] == player:
                 count += 1
 
         if player == 1:
@@ -110,23 +101,25 @@ class game_NineMensMorris:
             if count > self.black_mills:
                 return True
 
+        return False
+
     # return true if the parameter player have won and false else
-    def check_winner(self, player):
-        if player == 1 and (not self.legal_places_after(1) or self.agent_pieces < 3):
+    def check_winner(self):
+        if not self.legal_places_after(2) or self.agent_pieces < 3:
             return 1
-        if player == 2 and (not self.legal_places_after(2) or self.opp_pieces < 3):
+        if not self.legal_places_after(1) or self.opp_pieces < 3:
             return 2
         return 0
 
     # makes a random agent turn
     def agent_turn(self):
-        if self.opp_pieces == 3 and self.opp_pieces_notplaced == 0:
-            legal = self.flying_stage_moves(1)
+        if self.opp_pieces == 3 and self.opp_pieces_not_placed == 0:
+            legal = self.flying_stage_moves
             random_move = legal[rnd.randint(0, len(legal))]
             self.board[random_move[0][0]][random_move[0][1]] = 0
             self.board[random_move[1][0]][random_move[1][1]] = 1
             return
-        if self.opp_pieces_notplaced == 0:
+        if self.opp_pieces_not_placed == 0:
             legal = self.legal_places_after(1)
             random_move = legal[rnd.randint(0, len(legal))]
             print(random_move)
@@ -136,7 +129,9 @@ class game_NineMensMorris:
         legal = self.legal_places_before()
         random_move = legal[rnd.randint(0,len(legal))]
         self.board[random_move[0]][random_move[1]] = 1
-        self.agent_pieces_notplaced -= 1
+        self.agent_pieces_not_placed -= 1
+        if self.check_new_mills(1):
+            self.remove_opp_piece()
 
     # remove random opponent's piece
     def remove_opp_piece(self):
@@ -154,13 +149,13 @@ class game_NineMensMorris:
 
     # makes a random opponent turn
     def opp_turn(self):
-        if self.opp_pieces == 3 and self.opp_pieces_notplaced == 0:
-            legal = self.flying_stage_moves(2)
+        if self.opp_pieces == 3 and self.opp_pieces_not_placed == 0:
+            legal = self.flying_stage_moves
             random_move = legal[rnd.randint(0, len(legal))]
             self.board[random_move[0][0]][random_move[0][1]] = 0
             self.board[random_move[1][0]][random_move[1][1]] = 2
             return
-        if self.opp_pieces_notplaced == 0:
+        if self.opp_pieces_not_placed == 0:
             legal = self.legal_places_after(2)
             random_move = legal[rnd.randint(0, len(legal))]
             self.board[random_move[0][0]][random_move[0][1]] = 0
@@ -169,34 +164,35 @@ class game_NineMensMorris:
         legal = self.legal_places_before()
         random_move = legal[rnd.randint(0,len(legal))]
         self.board[random_move[0]][random_move[1]] = 2
-        self.opp_pieces_notplaced -= 1
+        self.opp_pieces_not_placed -= 1
+        if self.check_new_mills(1):
+            self.remove_opp_piece()
 
 class games:
-
     def __init__(self):
-        self.nmm = game_NineMensMorris() # object of the nine men's morris
-        self.amount_games = 1 # amount of games to run
-        self.white_wins = 0 # amount of wins for white
-        self.black_wins = 0 # amount of wins for black
+        self.nmm = game_NineMensMorris()  # object of the nine men's morris
+        self.amount_games = 10  # amount of games to run
+        self.white_wins = 0  # amount of wins for white
+        self.black_wins = 0  # amount of wins for black
 
     # play a single game of nine men's morris
     def single_game(self):
-        while nmm.check_winner() == 0:
-            nmm.agent_turn()
-            nmm.opp_turn()
-        return nmm.check_winner()
+        while self.nmm.check_winner() == 0:
+            self.nmm.agent_turn()
+            self.nmm.opp_turn()
+        return self.nmm.check_winner()
 
     # run a loop of the specified amount of games
-    def multipyle_games(self):
-        game_result = self.single_game()
+    def multiply_games(self):
         for i in range(self.amount_games):
+            game_result = self.single_game()
             if game_result == 1:
                 self.white_wins += 1
-            if game_result == 2:
+            elif game_result == 2:
                 self.black_wins += 1
 
 
-nmm = game_NineMensMorris()
-print(nmm.board)
-nmm.agent_turn()
-print(nmm.board)
+ninemensmorris = games()
+ninemensmorris.multiply_games()
+print("Wins for white:", ninemensmorris.white_wins)
+print("Wins for black:", ninemensmorris.black_wins)
